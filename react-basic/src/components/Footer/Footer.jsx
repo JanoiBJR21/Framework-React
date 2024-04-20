956f9101030d49e70b3e4e38e034bde048c27683
Apichat Wongtala <apichat26870@gmail.com>
2024-04-20 08:53:26 +0700

    add .

---
+++ react-basic/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <>
+      <div>
+        <p>© Janoi Inc.. 2024 All rights reserved.</p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ react-basic/src/components/Header/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="container">
+        <div className="con">
+          <div className="logo">
+            <h1>Janoi</h1>
+          </div>
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Sevices</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
