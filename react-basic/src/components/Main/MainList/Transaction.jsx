6befa430aced4cf119d40d84d40556caea182e3d
Apichat Wongtala <apichat26870@gmail.com>
2024-04-23 12:59:27 +0700

    Update Transaction.jsx
    การสร้าง props data

--- react-basic/src/components/Main/MainList/Transaction.jsx
+++ react-basic/src/components/Main/MainList/Transaction.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 import Item from './Item';
 import "./Transaction.css"
 const Transaction = () => {
+  const data = [
+    {title: "ค่ารักษาพยาบาล", amount:"2000"},
+    {title: "ค่ากระเป๋า", amount:"800"},
+    {title: "ค่าเดีนทาง", amount:"1200"},
+    {title: "ค่าเช่าบ้าน", amount:"9000"},
+    {title: "เงินเดือน", amount:"35000"},
+  ]
   return (
     <ul className='item-list'>
-      <Item title="ค่ารักษาพยาบาล" amount="2000"/>
-      <Item title="ค่ากระเป๋า" amount="500"/>
-      <Item title="ค่าเดีนทาง" amount="1000"/>
-      <Item title="ค่าเช่าบ้าน" amount="7000"/>
+      <Item title={data[0].title} amount={data[0].amount}/>
+      <Item title={data[1].title} amount={data[1].amount}/>
+      <Item title={data[2].title} amount={data[2].amount}/>
+      <Item title={data[3].title} amount={data[3].amount}/>
+      <Item title={data[4].title} amount={data[4].amount}/>
+
     </ul>
   );
 };
