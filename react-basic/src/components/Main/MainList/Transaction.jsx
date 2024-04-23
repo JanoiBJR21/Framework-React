f14cdf807a120b0cc75a98b4a227455af0eab7be
Apichat Wongtala <apichat26870@gmail.com>
2024-04-23 13:42:24 +0700

    commit Key Props UUID
    การใช้งาน Key Props UUID
    1. Install
    npm install uuid
    
    2. Create a UUID (ES6 module syntax)
    import { v4 as uuidv4 } from 'uuid';

--- react-basic/src/components/Main/MainList/Transaction.jsx
+++ react-basic/src/components/Main/MainList/Transaction.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Item from "./Item";
 import "./Transaction.css";
+import { v4 as uuidv4 } from "uuid";
+
 const Transaction = () => {
   const data = [
-    {id: 1, title: "ค่ารักษาพยาบาล", amount: "2000" },
-    {id: 2, title: "ค่ากระเป๋า", amount: "800" },
-    {id: 3, title: "ค่าเดีนทาง", amount: "1200" },
-    {id: 4, title: "ค่าเช่าบ้าน", amount: "9000" },
-    {id: 5, title: "เงินเดือน", amount: "35000" },
-    {id: 6, title: "ค่าประกันชีวิต", amount: "28000" },
+    { title: "ค่ารักษาพยาบาล", amount: "2000" },
+    { title: "ค่ากระเป๋า", amount: "800" },
+    { title: "ค่าเดีนทาง", amount: "1200" },
+    { title: "ค่าเช่าบ้าน", amount: "9000" },
+    { title: "เงินเดือน", amount: "35000" },
+    { title: "ค่าประกันชีวิต", amount: "28000" },
   ];
   return (
     <ul className="item-list">
       {data.map((element) => {
-        return <Item key = {element.id} {...element}/>;
+        return <Item key = {uuidv4()} {...element} />;
       })}
     </ul>
   );
