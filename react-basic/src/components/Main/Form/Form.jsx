c5e53005ac2368804d37b9ef654130a4946ed335
Apichat Wongtala <apichat26870@gmail.com>
2024-04-23 17:12:31 +0700

    Update Form.jsx
    การใช้งานของ Event และ ทำการตรวจจับ Event

--- react-basic/src/components/Main/Form/Form.jsx
+++ react-basic/src/components/Main/Form/Form.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
-import "./Form.css"
+import React from "react";
+import "./Form.css";
 
 const Form = () => {
+  const inputTitle = (e) => {
+    console.log(e.target.value);
+  };
+
+  const inputAmount = (e) => {
+    console.log(e.target.value);
+  };
+
+  const seveItem = (e) => {
+    e.preventDefault()
+    console.log("บันทึกข้อมูลเรียบร้อย");
+  };
   return (
     <div>
-        <form>
-            <div className='form-control'>
-                <label>ชื่อรายการ :</label>
-                <input type="text" placeholder='ระบุชื่อรายการารของคุณ'/>
-            </div>
-            <div className="form-control">
-                <label>จำนววนเงิน :</label>
-                <input type="number" placeholder='(+ รายรับ , - รายจ่าย)' />
-            </div>
-            <div className='btn-control'>
-                <button type="submit" className='btn'>เพิ่มข้อมูล</button>
-            </div>
-        </form>
+      <form onSubmit={seveItem}>
+        <div className="form-control">
+          <label>ชื่อรายการ :</label>
+          <input
+            type="text"
+            placeholder="ระบุชื่อรายการารของคุณ"
+            onChange={inputTitle}
+          />
+        </div>
+        <div className="form-control">
+          <label>จำนววนเงิน :</label>
+          <input
+            type="number"
+            placeholder="(+ รายรับ , - รายจ่าย)"
+            onChange={inputAmount}
+          />
+        </div>
+        <div className="btn-control">
+          <button type="submit" className="btn">
+            เพิ่มข้อมูล
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
