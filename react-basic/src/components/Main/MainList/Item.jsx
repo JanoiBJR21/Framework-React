c2f05b2dd664c76313ee14bc2fd6229f42acb79e
Apichat Wongtala <apichat26870@gmail.com>
2024-04-23 16:09:46 +0700

    commit file package-lock.json Item.jsx and Transaction.jsx
    Props Types
    // การตรวจสอบข้อมูลและกำหนดค่าให้ข้อมูล
    Item.propTypes = {
      title:PropTypes.string.isRequired,//title จะต้องเป็นข้อมูล String เท่านั้น และห้ามเป็นค่าว่าง
      amount:PropTypes.number.isRequired,//amount จะต้องเป็นข้อมูล Number เท่านั้น และห้ามเป็นค่าว่าง
    };

--- react-basic/src/components/Main/MainList/Item.jsx
+++ react-basic/src/components/Main/MainList/Item.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 const Item = (props) => {
-  const {title, amount} = props
+  const { title, amount } = props;
   return (
     <li>
-      {title}<span>{amount}</span>
+      {title}
+      <span>{amount}</span>
     </li>
   );
+};
+// การตรวจสอบข้อมูลและกำหนดค่าให้ข้อมูล
+Item.propTypes = {
+  title:PropTypes.string.isRequired,//title จะต้องเป็นข้อมูล String เท่านั้น และห้ามเป็นค่าว่าง
+  amount:PropTypes.number.isRequired,//amount จะต้องเป็นข้อมูล Number เท่านั้น และห้ามเป็นค่าว่าง
 };
 
 export default Item;
--- react-basic/src/components/Main/MainList/Transaction.jsx
+++ react-basic/src/components/Main/MainList/Transaction.jsx
@@ -5,12 +5,12 @@ import { v4 as uuidv4 } from "uuid";
 
 const Transaction = () => {
   const data = [
-    { title: "ค่ารักษาพยาบาล", amount: "2000" },
-    { title: "ค่ากระเป๋า", amount: "800" },
-    { title: "ค่าเดีนทาง", amount: "1200" },
-    { title: "ค่าเช่าบ้าน", amount: "9000" },
-    { title: "เงินเดือน", amount: "35000" },
-    { title: "ค่าประกันชีวิต", amount: "28000" },
+    { title: "ค่ารักษาพยาบาล", amount: 2000 },
+    { title: "ค่ากระเป๋า", amount: 800 },
+    { title: "ค่าเดีนทาง", amount: 1200 },
+    { title: "ค่าเช่าบ้าน", amount: 9000 },
+    { title: "เงินเดือน", amount: 35000 },
+    { title: "ค่าประกันชีวิต", amount: 28000 },
   ];
   return (
     <ul className="item-list">
