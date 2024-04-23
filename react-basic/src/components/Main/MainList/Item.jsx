a3592b2a3cf558f89a2bb22804d310693f2f0f91
Apichat Wongtala <apichat26870@gmail.com>
2024-04-23 13:07:36 +0700

    commit file Item.jsx
    การสร้าง และการเรียกใช้ Props Destructuring

--- react-basic/src/components/Main/MainList/Item.jsx
+++ react-basic/src/components/Main/MainList/Item.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-const Item = (props) => {
-  const {title, amount} = props
+const Item = ({title, amount}) => {
+  // const {title, amount} = props
   return (
     <li>
       {title}<span>{amount}</span>
