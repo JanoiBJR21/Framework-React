b59e4d31c59756fe6876a5de2a80eb29cb0259f5
Apichat Wongtala <apichat26870@gmail.com>
2024-04-23 13:03:06 +0700

    Update Item.jsx
    การสร้าง props Destructuring

--- react-basic/src/components/Main/MainList/Item.jsx
+++ react-basic/src/components/Main/MainList/Item.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 const Item = (props) => {
+  const {title, amount} = props
   return (
     <li>
-      {props.title}<span>{props.amount}</span>
+      {title}<span>{amount}</span>
     </li>
   );
 };
