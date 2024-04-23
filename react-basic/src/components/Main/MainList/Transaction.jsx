614d458eed92da3f605271af9a0de130c04d7c56
Apichat Wongtala <apichat26870@gmail.com>
2024-04-23 13:19:23 +0700

    Update Transaction.jsx
    การใช้งาน Props & Array Map
        <ul className="item-list">
          {data.map((element) => {
            return <Item title={element.title} amount={element.amount}/>;
          })}
        </ul>

--- react-basic/src/components/Main/MainList/Transaction.jsx
+++ react-basic/src/components/Main/MainList/Transaction.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import Item from './Item';
-import "./Transaction.css"
+import React from "react";
+import Item from "./Item";
+import "./Transaction.css";
 const Transaction = () => {
   const data = [
-    {title: "ค่ารักษาพยาบาล", amount:"2000"},
-    {title: "ค่ากระเป๋า", amount:"800"},
-    {title: "ค่าเดีนทาง", amount:"1200"},
-    {title: "ค่าเช่าบ้าน", amount:"9000"},
-    {title: "เงินเดือน", amount:"35000"},
-  ]
+    { title: "ค่ารักษาพยาบาล", amount: "2000" },
+    { title: "ค่ากระเป๋า", amount: "800" },
+    { title: "ค่าเดีนทาง", amount: "1200" },
+    { title: "ค่าเช่าบ้าน", amount: "9000" },
+    { title: "เงินเดือน", amount: "35000" },
+    { title: "ค่าประกันชีวิต", amount: "28000" },
+  ];
   return (
-    <ul className='item-list'>
-      <Item title={data[0].title} amount={data[0].amount}/>
-      <Item title={data[1].title} amount={data[1].amount}/>
-      <Item title={data[2].title} amount={data[2].amount}/>
-      <Item title={data[3].title} amount={data[3].amount}/>
-      <Item title={data[4].title} amount={data[4].amount}/>
-
+    <ul className="item-list">
+      {data.map((element) => {
+        return <Item title={element.title} amount={element.amount}/>;
+      })}
     </ul>
   );
 };
 
-export default Transaction
+export default Transaction;
