e6a1c4903fed1ee2a5179a5b8f10f11cff22e5b1
Apichat Wongtala <apichat26870@gmail.com>
2024-04-25 13:22:42 +0700

    Update MainList.jsx
    Update MainList.jsx

--- react-basic/src/components/Main/MainList/MainList.jsx
+++ react-basic/src/components/Main/MainList/MainList.jsx
@@ -6,11 +6,11 @@ import Form from "../Form/Form";
 const MainList = () => {
   const designh1 = { color: "red", fontSize: "25px", textAlign: "center" };
 
-  const initdata = [
-    { id: 1, title: "ค่ารักษาพยาบาล", amount: 2000 },
-    { id: 2, title: "ค่าเช่าบ้าน", amount: 9000 },
-    { id: 3, title: "เงินเดือน", amount: 35000 },
-  ];
+  // const initdata = [
+  //   { id: 1, title: "ค่ารักษาพยาบาล", amount: 2000 },
+  //   { id: 2, title: "ค่าเช่าบ้าน", amount: 9000 },
+  //   { id: 3, title: "เงินเดือน", amount: 35000 },
+  // ];
 
   const [items, setItems] = useState([]);
 
