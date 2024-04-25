0e9f3c17c51efdac9a2910398a9bbedf8689c652
Apichat Wongtala <apichat26870@gmail.com>
2024-04-25 13:45:34 +0700

    commit fille Item.jsx.css
    การตกแต่ง Item Style

--- react-basic/src/components/Main/MainList/Item.jsx
+++ react-basic/src/components/Main/MainList/Item.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Item.css"
 
 const Item = (props) => {
   const { title, amount } = props;
+
+  const status = amount < 0 ? "expense" : "income";
+  const symbol = amount < 0 ? "-" : "+";
   return (
-    <li>
+    <li className={status}>
       {title}
-      <span>{amount}</span>
+      <span>{symbol}{Math.abs(amount)}</span>
     </li>
   );
 };
 // การตรวจสอบข้อมูลและกำหนดค่าให้ข้อมูล
 Item.propTypes = {
-  title:PropTypes.string.isRequired,//title จะต้องเป็นข้อมูล String เท่านั้น และห้ามเป็นค่าว่าง
-  amount:PropTypes.number.isRequired,//amount จะต้องเป็นข้อมูล Number เท่านั้น และห้ามเป็นค่าว่าง
+  title: PropTypes.string.isRequired, //title จะต้องเป็นข้อมูล String เท่านั้น และห้ามเป็นค่าว่าง
+  amount: PropTypes.number.isRequired, //amount จะต้องเป็นข้อมูล Number เท่านั้น และห้ามเป็นค่าว่าง
 };
 
 export default Item;
