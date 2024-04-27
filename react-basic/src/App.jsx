47f61f0ce3d2a02b0b1e1f79579595bea61a61c5
Apichat Wongtala <apichat26870@gmail.com>
2024-04-27 15:30:46 +0700

    commit

--- react-basic/src/App.jsx
+++ react-basic/src/App.jsx
@@ -1,21 +1,21 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Header/Navbar'
-import Main from './components/Main/Main'
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Header/Navbar";
+import Main from "./components/Main/Main";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Navbar/>
-      <Main/>
-      <Footer/>
+      <Navbar />
+      <Main />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
